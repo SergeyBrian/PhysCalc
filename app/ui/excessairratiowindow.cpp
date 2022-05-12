171d9e72d90f6a43bd5620389393b1f07b17bbd6
Sergey <s.brianski@yahoo.com>
2022-05-12 22:49:07 +0300

    Add calculator calling to excess air ratio window

--- app/ui/excessairratiowindow.cpp
+++ app/ui/excessairratiowindow.cpp
@@ -22,69 +22,22 @@ void ExcessAirRatioWindow::loadTable(QString key) {
 
 }
 
-void ExcessAirRatioWindow::on_toolButton_clicked()
-{
-     this->openCalculator(Calculators::EXCESS_AIR_RATIO);
-}
-
 void ExcessAirRatioWindow::on_toolButton_2_clicked()
 {
-    this->openCalculator(Calculators::HEATING_VALUE);
+    ui->lineEdit_3->setText(QString::number(CalculatorInterface::calculate(Calculators::HEATING_VALUE, storage_)));
 }
 
 void ExcessAirRatioWindow::on_toolButton_3_clicked()
 {
-    this->openCalculator(Calculators::HEAT_CAPACITY);
+    ui->lineEdit_7->setText(QString::number(CalculatorInterface::calculate(Calculators::HEAT_CAPACITY, storage_)));
 }
 
 void ExcessAirRatioWindow::on_toolButton_4_clicked()
 {
-    this->openCalculator(Calculators::HEAT_CAPACITY);
+    ui->lineEdit_8->setText(QString::number(CalculatorInterface::calculate(Calculators::HEAT_CAPACITY, storage_)));
 }
 
 void ExcessAirRatioWindow::on_toolButton_5_clicked()
 {
-    this->openCalculator(Calculators::HEATING_VALUE);
-}
-
-
-
-void ExcessAirRatioWindow::on_pushButton_clicked()
-{
-     this->loadTable("a");
-}
-
-void ExcessAirRatioWindow::on_pushButton_2_clicked()
-{
-    this->loadTable("Q_l_");
-}
-
-void ExcessAirRatioWindow::on_pushButton_3_clicked()
-{
-    this->loadTable("L_0");
-}
-
-void ExcessAirRatioWindow::on_pushButton_4_clicked()
-{
-    this->loadTable("k_cc");
-}
-
-void ExcessAirRatioWindow::on_pushButton_5_clicked()
-{
-    this->loadTable("e_cc");
-}
-
-void ExcessAirRatioWindow::on_pushButton_6_clicked()
-{
-    this->loadTable("t");
-}
-
-void ExcessAirRatioWindow::on_pushButton_7_clicked()
-{
-    this->loadTable("C_pmi_A");
-}
-
-void ExcessAirRatioWindow::on_pushButton_8_clicked()
-{
-    this->loadTable("C_pmi");
+    ui->lineEdit_2->setText(QString::number(CalculatorInterface::calculate(Calculators::HEAT_CAPACITY, storage_)));
 }
--- app/ui/excessairratiowindow.h
+++ app/ui/excessairratiowindow.h
@@ -19,7 +19,6 @@ public:
     ~ExcessAirRatioWindow();
 
 private slots:
-    void on_toolButton_clicked();
 
     void on_toolButton_2_clicked();
 
@@ -27,24 +26,7 @@ private slots:
 
     void on_toolButton_4_clicked();
 
-    void on_pushButton_clicked();
-
-    void on_pushButton_2_clicked();
-
     void on_toolButton_5_clicked();
-
-    void on_pushButton_3_clicked();
-
-    void on_pushButton_4_clicked();
-
-    void on_pushButton_5_clicked();
-
-    void on_pushButton_6_clicked();
-
-    void on_pushButton_7_clicked();
-
-    void on_pushButton_8_clicked();
-
 private:
     Ui::ExcessAirRatioWindow *ui;
     void openCalculator(Calculators::Calculator c);
