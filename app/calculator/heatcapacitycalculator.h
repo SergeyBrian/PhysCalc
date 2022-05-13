c152009d4b56607124c2f931e9317348800b4d56
Kudukdk <94736132+Kudukdk@users.noreply.github.com>
2022-05-13 21:10:53 +0300

    rtdfdglkjhgfd

--- app/calculator/heatcapacitycalculator.h
+++ app/calculator/heatcapacitycalculator.h
@@ -2,12 +2,19 @@
 #define HEATCAPACITYCALCULATOR_H
 
 #include "calculator.h"
+#include <cmath>
 
 class HeatCapacityCalculator : public Calculator
 {
 public:
     HeatCapacityCalculator(DataStorage * storage);
     double calculate() override;
+private:
+    void T_szh();
+    void T_rassh();
+    void R_smesi();
+    void end_rassh();
+    void end_szh();
 };
 
 #endif // HEATCAPACITYCALCULATOR_H
