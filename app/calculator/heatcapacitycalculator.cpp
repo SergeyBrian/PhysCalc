93ffa1ed9ee79c16289cb0f3f3649729914e7e0d
MaxSyr <mmsyrnikov@gmail.com>
2022-05-13 13:13:54 +0300

    CByTempCalcDone

--- app/calculator/heatcapacitycalculator.cpp
+++ app/calculator/heatcapacitycalculator.cpp
@@ -1,5 +1,6 @@
 #include "heatcapacitycalculator.h"
 #include "../exceptions/dataexceptions.h"
+#include <cmath>
 
 HeatCapacityCalculator::HeatCapacityCalculator(DataStorage * storage) : Calculator(storage)
 {
@@ -9,3 +10,27 @@ double HeatCapacityCalculator::calculate()
 {
     return 0;
 }
+
+void HeatCapacityCalculator::calculateC_pmi_p(QString key)
+{
+    storage->setValue(key, $(""))
+}
+
+void HeatCapacityCalculator::calculateByTemp(QString key)
+{
+    auto T = storage->getValue<std::map<double, double>>("T");
+
+
+    int c=0;
+    int p=0;
+    for (auto const & element : T)
+    {
+        if ($("T")<element.first)
+        {
+            storage->setValue(key, c+(($("T")-p)/(element.first-p)*(element.second-c)));
+        }
+        c = element.second;
+        p = element.first;
+    }
+}
+
